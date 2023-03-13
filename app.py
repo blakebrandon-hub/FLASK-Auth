c43409b0ff2a48c69c2c929b16d2111ae208a926
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2023-03-13 07:31:35 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:\\Users\\18313\\Desktop\\database.db"
+Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data,
+                                                 method='sha256')
+
+        username = form.username.data.rstrip()
+        email = form.email.data.rstrip()
+
+        new_user = User(username=username, email=email,
+                        password=hashed_password, admin=0)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user:
+
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+
+                return redirect(url_for('index'))
+
+        return "<h2>Error: Invalid username or password</h2>"
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="8000", debug=True)
